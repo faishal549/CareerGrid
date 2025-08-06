62a84b3c4643e4c1e096b5c2a9a43e98ea7d8bb1
[faishal saifi] <[fslazim@gmail.com]>
2025-08-06 20:46:12 +0530

    styling

--- Frontend/src/Components/ResumeUI.jsx
+++ Frontend/src/Components/ResumeUI.jsx
@@ -56,7 +56,7 @@ const ResumeUI = ({ userResume }) => {
                     </li>
                 </ul>
                 {showConfirm && (
-                    <div role="alert" className="alert alert-vertical sm:alert-horizontal">
+                    <div role="alert" className="fixed top-10 left-1/2 transform -translate-x-1/2 z-50 w-full max-w-md px-4">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info h-6 w-6 shrink-0">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                         </svg>
